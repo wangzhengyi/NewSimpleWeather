fdafe932775d937101ecb094d3565371a465db07
wangzhengyi <wzyll1314@gmail.com>
2015-11-19 17:29:56 +0800

    Fix class name error

---
+++ src/main/java/com/melhc/model/Weather.java
@@ -0,0 +1,13 @@
+package com.melhc.model;
+
+public class Weather {
+	private WeatherInfo weatherinfo;
+
+	public WeatherInfo getWeatherinfo() {
+		return weatherinfo;
+	}
+
+	public void setWeatherInfo(WeatherInfo weatherinfo) {
+		this.weatherinfo = weatherinfo;
+	}
+}
